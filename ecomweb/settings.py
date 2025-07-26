f2e426e57ac21dc284137efc1471fd809a6a1652
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2025-07-26 14:51:47 +0530

    Update settings.py

--- ecomweb/settings.py
+++ ecomweb/settings.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
@@ -63,9 +61,6 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
     }
 }
 
